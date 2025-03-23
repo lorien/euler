9ac38be3609df1cf038274adef1a48c11c534d74
User <guest@example.com>
2025-03-23 17:44:25 +0300

    Improve style of solution 3

--- examples/problem3.rs
+++ examples/problem3.rs
@@ -23,10 +23,30 @@ fn solution_common() -> i64 {
             }
         }
     }
-    match result {
-        None => panic!("Could not find prime factors of number {}", target),
-        Some(val) => val,
+    result.unwrap()
+}
+
+fn solution_with_division() -> i64 {
+    let target = TARGET;
+    assert!(target >= 2);
+    let mut result = None;
+    let mut primes = HashSet::new();
+    for num in 2..=((target as f64).sqrt().floor() as i64) {
+        if target % num == 0 {
+            let mut is_prime = true;
+            for prime in &primes {
+                if num % prime == 0 {
+                    is_prime = false;
+                    break;
+                }
+            }
+            if is_prime {
+                primes.insert(num);
+                result = Some(num);
+            }
+        }
     }
+    result.unwrap()
 }
 
 fn main() {
