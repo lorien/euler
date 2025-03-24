f87eeb891fdad91a379393bf095ec3c771cea3e2
User <guest@example.com>
2025-03-24 15:40:31 +0300

    Add another solution for problem 7

--- examples/problem7.rs
+++ examples/problem7.rs
@@ -31,6 +31,36 @@ fn solution_common() -> i64 {
     target_prime
 }
 
+fn solution_another() -> i64 {
+    let target_prime_idx = TARGET;
+    assert!(target_prime_idx > 0);
+    let mut prime_idx = 1;
+    let mut num = 2;
+    let mut target_prime = num;
+    while prime_idx < target_prime_idx {
+        num += if num == 2 { 1 } else { 2 };
+        let mut is_prime = true;
+        if num != 3 && num % 3 == 0 {
+            continue;
+        }
+        let num_sqrt = (num as f64).sqrt().floor() as i64;
+        let mut factor = 5;
+        while factor <= num_sqrt {
+            if num % factor == 0 || num % (factor + 2) == 0 {
+                is_prime = false;
+                break;
+            }
+            factor += 6;
+        }
+        if is_prime {
+            target_prime = num;
+            prime_idx += 1;
+        }
+    }
+    target_prime
+}
+
 fn main() {
     check_solution(7, "common", &solution_common);
+    check_solution(7, "another", &solution_another);
 }
